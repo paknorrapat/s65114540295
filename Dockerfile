3a89fb7984e4fececbaa560f1ac77b7821ce2246
paknorrapat <norrapat.pa.65@ubu.ac.th>
2025-07-08 13:12:28 +0700

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
 FROM python:3.12
 
+RUN apt-get update && \
+    apt-get install -y curl && \
+    curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
+    apt-get install -y nodejs && \
+    npm install -g npm@latest && \
+    apt-get clean && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /app
 
-COPY . /app/
+COPY . .
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
 
+WORKDIR /app/theme/static_src
+RUN npm install
 
-EXPOSE 8000
+WORKDIR /app
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["sh", "-c", "python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
--- myproject/settings.py
+++ myproject/settings.py
@@ -87,28 +87,36 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {   
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'sparky',
-#         'USER': 'sparkyuser',
-#         'PASSWORD': '1234',
-#         'HOST': 'db',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {   
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sparky',
         'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'PASSWORD': 'root',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
 
+# DATABASES = {   
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sparky',
+#         'USER': 'root',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'sqlite3': {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     },
+#     }
+# }
 
 
 # Password validation
